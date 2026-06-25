fface999b56bed5f3ff5cdbdae27e166a48436cb
krosop <krosop@github.com>
2026-06-25 03:38:21 +0300

    fix(api): use static import with Vercel adapter after removing dotenv

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,4 @@
 import { handle } from "@hono/node-server/vercel";
+import { app } from "../server/app";
 
-let app: any;
-let error: any;
-
-async function init() {
-  try {
-    const mod = await import("../server/app");
-    app = mod.app;
-  } catch (e: any) {
-    error = e;
-  }
-}
-
-const initPromise = init();
-
-export default async function (req: any, res: any) {
-  await initPromise;
-  
-  if (error) {
-    res.statusCode = 500;
-    res.end(JSON.stringify({ error: error.message, stack: error.stack }));
-    return;
-  }
-  
-  return handle(app)(req, res);
-}
+export default handle(app);
