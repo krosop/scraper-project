ec32fa52dd6c8ca157771cd2c7a6bfb04a5a16cf
krosop <krosop@github.com>
2026-06-25 03:22:55 +0300

    debug(api): use @hono/node-server/vercel adapter

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
-import { getRequestListener } from "@hono/node-server";
+import { handle } from "@hono/node-server/vercel";
 import { Hono } from "hono";
 
 const app = new Hono();
 app.get("/api/trpc/ping", (c) => c.json({ ok: true, ts: Date.now() }));
 
-export default getRequestListener(app);
+export default handle(app);
